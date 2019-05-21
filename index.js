5c26f0ae29b63f96e0b9c6126dcf5281aea556b1
Bob McDouble <bobmcdouble3@gmail.com>
2019-05-21 18:12:08 -0400

    d

---
+++ index.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+
+var htmlPath = path.join(__dirname, 'src');
+
+app.use(express.static(htmlPath));
+
+var server = app.listen(80, function () {
+    var host = 'localhost';
+        var port = server.address().port;
+            console.log('listening on http://'+host+':'+port+'/');
+            });
